889f3ef58edbad68cc9d17d54a48694869add6af
Wexlersolk~ <danylkarp@gmail.com>
2023-10-05 12:38:20 +0300

    cleared comments

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 
-// Replace 'your-input.json' with the path to your JSON file.
 const inputFile = 'data.json';
 
-// Read the JSON file.
 fs.readFile(inputFile, 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading the JSON file:', err);
@@ -11,19 +9,14 @@ fs.readFile(inputFile, 'utf8', (err, data) => {
   }
 
   try {
-    // Parse the JSON data.
     const jsonData = JSON.parse(data);
 
-    // Extract the necessary data.
     const necessaryData = jsonData.map(item => `${item.exchangedate}:${item.rate}`);
 
-    // Convert the extracted data to a string.
     const outputData = JSON.stringify(necessaryData, null, 2);
 
-    // Specify the path for the output file (output.txt).
     const outputFile = 'output.txt';
 
-    // Write the extracted data to the output.txt file.
     fs.writeFile(outputFile, outputData, 'utf8', err => {
       if (err) {
         console.error('Error writing to the output file:', err);
